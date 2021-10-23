7e2aceba56937da0b72ec8e155bba3e391c0eed1
mikeyTG <88639357+mikeyTG@users.noreply.github.com>
2021-10-23 08:20:08 +0530

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -81,7 +81,7 @@ async def start_(client: Client, message: Message):
                 ],
                 [
                     InlineKeyboardButton(
-                        "🌐 DEVELOPER", url="https://t.me/charmy_anime"
+                        "🌐 DEVELOPER", url="https://t.me/charmyanime"
                     )
                 ],
             ]
